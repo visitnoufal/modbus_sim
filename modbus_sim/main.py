3ce2a68b92d2c60526cf7016acfe0e025351fd44
visitnoufal <visitnoufal@gmail.com>
2018-03-06 07:04:48 +0530

    Update main.py

--- modbus_sim/main.py
+++ modbus_sim/main.py
@@ -146,7 +146,7 @@ class ModbusServer(object):
             # data.append(str(slave_to_add))
             
     def set_rand_values(self, slave_to_add, block_type, block_start, bock_size, modbus_settings={}):
-        threading.Timer(2.0, set_rand_values).start()
+        threading.Timer(2.0, set_rand_values(self, slave_to_add, block_type, block_start, bock_size, modbus_settings={})).start()
         for x in range(block_start, block_size):
             self.modbus_device.set_values(slave_to_add, block_name, x, [randint(200,250)] * 1)
 
